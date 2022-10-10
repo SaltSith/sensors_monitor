9d69bed63b859941b2d2bb3b24d1ae52d768b1cb
SaltSith66 <SaltSith66@gmail.com>
2022-10-10 22:20:28 +0200

    include and run zmq_plugin

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "monitor/monitor.h"
+#include "zmq_plugin/zmq_plugin_task.h"
 
 int 
 main(void)
@@ -9,7 +10,12 @@ main(void)
         return -1;
     }
 
+    if (zmq_plugin_task_init()) {
+        return -1;
+    }
+
     pthread_join(monitor_task_handle_get(), NULL);
+    pthread_join(zmq_plugin_task_handle_get(), NULL);
 
     return 0;
 }
